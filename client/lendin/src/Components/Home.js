1985a894cfde70cfcecc1985489fbff0850191a7
Aryan7440 <aryanshukla744@gmail.com>
2024-02-25 05:31:00 +0530

    improved UI of homepage,navbar and added login/logout functionality
    - Implemented CSS styles for the homepage, navbar, and various other components to enhance the visual appearance and user experience.
    - Added styling for anchor tags, buttons, and hero links to make them more visually appealing and interactive.
    - Integrated login functionality, allowing users to log in to the application securely.
    - Updated CSS classes and styles to ensure consistency and coherence across the application.
    - Reviewed and optimized code for better readability, maintainability, and performance.

--- client/lendin/src/Components/Home.js
+++ client/lendin/src/Components/Home.js
@@ -1,19 +1,29 @@
-import Hero from './Hero'
-import './css/page.css'
-
+import './css/home.css'
+import heroImage from '../images/modi.jpg'
 const Home = () => {
   return (
     <div className="page">
       <div className="home section">
-        <Hero></Hero>
-        <h1 className="header">LendIn : Lend/Borrow Money instantly</h1>
-
-        <a href="/register_inv">New to Investing, Register here</a>
-
-        <a href="/register_brw">Want Instant Cash! Register-Apply-Get_Money</a>
-
-        <h2 className="subheader">Login As borrower</h2>
-        <a href="/Login">Already a User, Login</a>
+        <div className="hero-container">
+          <div className="hero-content">
+            <img src={heroImage} alt="Hero" className="hero-image" />
+            <div className="hero-text">
+              <h1 className="header">
+                LendIn : Lend or Borrow Money instantly
+              </h1>
+              <a href="/register_inv" className="hero-link">
+                New to Investing, Register here
+              </a>
+              <a href="/register_brw" className="hero-link">
+                Want Instant Cash! Register-Apply-Get_Money
+              </a>
+              <h2 className="subheader">Login As borrower</h2>
+              <a href="/Login" className="hero-link">
+                Already a User, Login
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- client/lendin/src/Components/Register_Borrower.js
+++ client/lendin/src/Components/Register_Borrower.js
@@ -1,72 +1,94 @@
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 const Register_Borrower = () => {
-    const navigate=useNavigate();
-    const HandleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-                const serverResponse = await axios.post("http://localhost:3001/Register_Borrower", {
-                name: e.target.borrower_name.value,
-                contact_number: e.target.borrower_contact_number.value,
-                email:e.target.borrower_email.value,
-                password:e.target.borrower_password.value,
-                address: e.target.borrower_address.value,
-                aadhar :e.target.borrower_aadhar.value,
-                pan: e.target.borrower_pan.value,
-                sex: e.target.borrower_sex.value,
-                pan_image:e.target.borrower_pan_image.files[0],// Access file data using files[0]
-                aadhar_image: e.target.borrower_aadhar_image.files[0] 
-            });
-            // console.log(serverResponse.data);
-            if (serverResponse.data.is_true === true) {
-                console.log("Success");
-                navigate('/Login');
-            } else {
-                console.log('Try Again, Error');
-            }
-        } catch (error) {
-            console.log(error);
+  const navigate = useNavigate()
+  const HandleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const serverResponse = await axios.post(
+        'http://localhost:3001/Register_Borrower',
+        {
+          name: e.target.borrower_name.value,
+          contact_number: e.target.borrower_contact_number.value,
+          email: e.target.borrower_email.value,
+          password: e.target.borrower_password.value,
+          address: e.target.borrower_address.value,
+          aadhar: e.target.borrower_aadhar.value,
+          pan: e.target.borrower_pan.value,
+          sex: e.target.borrower_sex.value,
+          pan_image: e.target.borrower_pan_image.files[0], // Access file data using files[0]
+          aadhar_image: e.target.borrower_aadhar_image.files[0],
         }
-    };
-    return (
-        <div>
-            <form onSubmit={HandleSubmit} >
-                <label for="borrower_name">Name:</label>
-                <input type="text" id="borrower_name" name="borrower_name"/><br/>
+      )
+      // console.log(serverResponse.data);
+      if (serverResponse.data.is_true === true) {
+        console.log('Success')
+        navigate('/Login')
+      } else {
+        console.log('Try Again, Error')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <div className="page">
+      <div className="section">
+        <form className="info-form" onSubmit={HandleSubmit}>
+          <label for="borrower_name">Name:</label>
+          <input type="text" id="borrower_name" name="borrower_name" />
 
-                <label for="borrower_contact_number">Contact Number:</label>
-                <input type="text" id="borrower_contact_number" name="borrower_contact_number"/><br/>
+          <label for="borrower_contact_number">Contact Number:</label>
+          <input
+            type="text"
+            id="borrower_contact_number"
+            name="borrower_contact_number"
+          />
 
-                <label for="borrower_email">Email:</label>
-                <input type="email" id="borrower_email" name="borrower_email"/><br/>
+          <label for="borrower_email">Email:</label>
+          <input type="email" id="borrower_email" name="borrower_email" />
 
-                <label for="borrower_password">Password:</label>
-                <input type="password" id="borrower_password" name="borrower_password"/><br/>
+          <label for="borrower_password">Password:</label>
+          <input
+            type="password"
+            id="borrower_password"
+            name="borrower_password"
+          />
 
-                <label for="borrower_address">Address:</label>
-                <input type="text" id="borrower_address" name="borrower_address"/><br/>
+          <label for="borrower_address">Address:</label>
+          <input type="text" id="borrower_address" name="borrower_address" />
 
-                <label for="borrower_aadhar">Aadhar Number:</label>
-                <input type="text" id="borrower_aadhar" name="borrower_aadhar"/><br/>
+          <label for="borrower_aadhar">Aadhar Number:</label>
+          <input type="text" id="borrower_aadhar" name="borrower_aadhar" />
 
-                <label for="borrower_pan">PAN Number:</label>
-                <input type="text" id="borrower_pan" name="borrower_pan"/><br/>
+          <label for="borrower_pan">PAN Number:</label>
+          <input type="text" id="borrower_pan" name="borrower_pan" />
 
-                <label for="borrower_sex">Sex:</label>
-                <select id="borrower_sex" name="borrower_sex">
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-                </select><br/>
+          <label for="borrower_sex">Sex:</label>
+          <select id="borrower_sex" name="borrower_sex">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
 
-                <label for="borrower_pan_image">PAN Image:</label>
-                <input type="file" id="borrower_pan_image" name="borrower_pan_image"/><br/>
+          <label for="borrower_pan_image">PAN Image:</label>
+          <input
+            type="file"
+            id="borrower_pan_image"
+            name="borrower_pan_image"
+          />
 
-                <label for="borrower_aadhar_image">Aadhar Image:</label>
-                <input type="file" id="borrower_aadhar_image" name="borrower_aadhar_image"/><br/>
+          <label for="borrower_aadhar_image">Aadhar Image:</label>
+          <input
+            type="file"
+            id="borrower_aadhar_image"
+            name="borrower_aadhar_image"
+          />
 
-            <button type="submit">Submit</button>
+          <button type="submit">Submit</button>
         </form>
+      </div>
     </div>
-)};
-export default Register_Borrower;
+  )
+}
+export default Register_Borrower
